54dc96ae0b75062cb6826f730b0b71a1d5efa173
George Berezhnoy <gohabereg@gmail.com>
2018-05-08 15:07:05 +0300

    Fix

--- Equations/bruteforce.py
+++ Equations/bruteforce.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
 
 
 class Bruteforce:
--- Equations/main.py
+++ Equations/main.py
@@ -1,3 +1,4 @@
+import numpy as np
 from math import sin
 from bisect import Bisect
 from secant import Secant
@@ -5,14 +6,14 @@ from bruteforce import Bruteforce
 
 
 def f(x):
-    return x**2 + 4*sin(x)
+    return x**2 + 4*np.sin(x)
 
 
 left = int(input('Enter left border: '))
 right = int(input('Enter right border: '))
 n = int(input('Enter number of points: '))
 
-bf = Bruteforce(left, right, n, func)
+bf = Bruteforce(left, right, n, f)
 bf.FirstStep()
 bf.plots()
 
@@ -21,5 +22,5 @@ s2 = Secant.find(f, a=-1, b=1)
 b1 = Bisect.find(f, a=-2, b=-1)
 b2 = Bisect.find(f, a=-1, b=1)
 
-print("Secant: {} {}".format(s1, s2))
+print("\nSecant: {} {}".format(s1, s2))
 print("Bisect: {} {}".format(b1, b2))
